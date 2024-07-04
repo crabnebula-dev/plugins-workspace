aba6fae08fe2e0d3595f3373689fe50e3ff3f3a5
pyranota <pyra@duck.com>
2024-07-04 18:03:06 +0200

    Make example know where you are and display it.

--- examples/api/src/views/Geolocation.svelte
+++ examples/api/src/views/Geolocation.svelte
@@ -1,20 +1,46 @@
 <script>
   import { MapLibre, DefaultMarker } from "svelte-maplibre";
+  import { watchPosition } from "@tauri-apps/plugin-geolocation";
 
+  // Default location
   // Alexander platz, berlin
   let marker = {
     lon: 13.413244,
     lat: 52.521992,
   };
+
+  // TODO: Stop watching once view exited
+  watchPosition(
+    {
+      enableHighAccuracy: true,
+      timeout: 0,
+      maximumAge: 500,
+    },
+    (e) => {
+      if (typeof e === "string") {
+        // TODO: Print to console
+        alert(e);
+      } else {
+        var coords = e.coords;
+        var lon = coords.longitude;
+        var lat = coords.latitude;
+        marker.lon = lon;
+        marker.lat = lat;
+      }
+    },
+  )
+    // TODO: Print to console
+    .then((e) => alert(e))
+    .catch((e) => alert(e));
 </script>
 
 <MapLibre
-  center={[13.413244, 52.521992]}
+  center={marker}
   zoom={14}
   class="map"
   style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
   interactive={false}
-  bearing={190}
+  bearing={0}
 >
   ▔ <DefaultMarker lngLat={marker}></DefaultMarker>
 </MapLibre>
